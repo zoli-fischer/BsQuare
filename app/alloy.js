a84376a9ad17d2a54b4ea94e8943bba91ba12d72
Zoltan L. Fischer <zoli_fischer@yahoo.com>
2015-10-14 19:28:54 +0200

    v1.0c

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,148 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+//show activity indicator
+Alloy.Globals.activityIndicatorWnd = (function() {
+	var self = this;
+	
+	//hide indicator
+	self.hide = function() {
+		//close wnd
+		self.activityIndicatorWin.close();
+		
+		//release memory
+		self.delete();
+	};
+	
+	//show indicator
+	self.show = function( obj ) {
+		
+		//set defaults
+		obj.message = typeof obj.message === 'undefined' ? 'Loading...' : obj.message;
+		obj.onBack = typeof obj.onBack === 'undefined' ? null : obj.onBack;
+		
+		//set up activity indicator wnd
+		self.activityIndicatorWin = Ti.UI.createWindow({
+	 		backgroundColor: 'white',
+	  		fullscreen: true,
+		    modal: true,
+		    tabBarHidden: true,
+		    navBarHidden: true
+		});
+	
+		//set up activity indicator
+		self.activityIndicator = Ti.UI.createActivityIndicator({
+		  color: 'black',
+		  indicatorColor: 'black',
+		  font: { fontFamily:'Helvetica Neue', fontSize: 18, fontWeight: 'normal' },
+		  message: obj.message,
+		  style: Ti.Platform.name === 'iPhone OS' ? Ti.UI.iPhone.ActivityIndicatorStyle.DARK : Ti.UI.ActivityIndicatorStyle.DARK,	  
+		  height: Ti.UI.SIZE,
+		  width: Ti.UI.SIZE
+		});
+		
+		//
+		if ( self.activityIndicatorWin && self.activityIndicator )
+			self.activityIndicatorWin.add(self.activityIndicator);
+			
+		//on open wnd show indicator
+		if ( self.activityIndicatorWin )
+			self.activityIndicatorWin.addEventListener('open', function (e) {
+			 	//show activity indicator
+			 	if ( self.activityIndicator )
+					self.activityIndicator.show();	  
+			 	
+			 	//hide activity bar 
+			 	if ( Ti.Platform.name === "android" && this && this.activity )
+				  	this.activity.actionBar.hide();
+				
+			});
+		
+		//on android back cancel activity
+		if ( self.activityIndicatorWin )
+			self.activityIndicatorWin.addEventListener('android:back',function(e) {
+				
+				//run cancel callback
+				if ( typeof obj.onBack == 'function' ) {
+					var r = obj.onBack.call( self );
+					if ( typeof r != 'undefined' )
+						return r;
+				}	
+				
+				/*
+				this.fireEvent('activity_canceled', {});
+				if (webview.canGoBack()) {
+			        webview.goBack();
+			    } else {
+			       a.show(); // show the leaving dialog
+			    }
+			    return false;
+				*/
+			});
+		
+		//open wnd
+		if ( self.activityIndicatorWin ) 
+			self.activityIndicatorWin.open();
+	};
+	
+	//delete object
+	self.delete = function() {
+		//remove child from wnd
+		self.activityIndicatorWin.remove(activityIndicator);
+		
+		//release memory
+		delete self.activityIndicatorWin;	
+		delete self.activityIndicator;	
+	};
+	
+	return self;
+})();
+
+//set up window action bar
+Alloy.Globals.setActionBar = function( view, showBack ) {
+	if ( Ti.Platform.name === "android" && view && view.activity ) {
+		var actionBar = view.activity.actionBar;
+		
+		//get showBack value
+		showBack = typeof showBack === 'undefined' ? false : showBack;
+		
+		if ( showBack ) {
+			//show back button
+			actionBar.setDisplayHomeAsUp(true);
+			
+			//go back
+			actionBar.onHomeIconItemSelected = function() {
+			    Ti.Android.currentActivity.finish();
+			};
+		};
+		
+		//show activity bar
+		actionBar.show();
+		
+		//set title
+		actionBar.title = 'BsQuare Audio Test';
+		
+		//create activity bar menu
+		view.activity.onCreateOptionsMenu = function(e) { 
+			var menuItemHelp = e.menu.add({ 
+				title : "Help", 
+				icon : "", 
+				showAsAction : Ti.Android.SHOW_AS_ACTION_WITH_TEXT 
+			});
+			menuItemHelp.addEventListener("click", function(e) { 
+				alert('Help'); 
+			}); 
+			
+			var menuItemAbout = e.menu.add({ 
+				title : "About", 
+				icon : "", 
+				showAsAction : Ti.Android.SHOW_AS_ACTION_WITH_TEXT 
+			});
+			menuItemAbout.addEventListener("click", function(e) { 
+				alert('About'); 
+			}); 
+		};
+	
+	};
+};
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,117 @@
-//include helper functions
-Ti.include('helper.js');
+//Check if string is valid cpr number
+function isValidCPR( cpr ) {
+	if ( typeof cpr != 'string' )
+		return false;
+	
+	//trim string	
+	cpr = cpr.trim();
+	 
+	//should have the dash
+	if ( cpr.length == 11 )
+		if ( cpr.indexOf('-') == 6 ) {
+			cpr = cpr.replace('-','');
+		} else {
+			return false;
+		}
+	
+	if ( cpr.length != 10 )
+		return false;
+	
+	//is only numbers
+	if ( !cpr.match(/^[0-9]+$/) )
+		return false;
+	
+	var day = parseInt(cpr.substr(0, 2), 10),
+    	month = parseInt(cpr.substr(2, 2), 10) - 1,
+    	year = parseInt(cpr.substr(4, 2), 10),
+    	date = new Date(year, month, day);
+    	
+	//validate date
+    if ( date.getYear() !== year || date.getMonth() !== month || date.getDate() !== day )
+        return false;
+        
+    return true;
+};
 
-//on submit form
-function onSubmit() {
-	if ( isValidCPR( $.cpr_number.value ) ) {
-		alert('Valid CPR number.');
+$.cpr_number.value = '1510833607';
+//continue with cpr valid or not
+function cprValidated( valid ) {
+	
+	//hide activity indicator
+	Alloy.Globals.activityIndicatorWnd.hide();
+	
+	//check if valid
+	if ( valid ) {
+		alert('CPR number is valid.');
 	} else {
 		$.cpr_number.focus();
 		alert('Please insert a valid CPR number.');
+	};
+	
+};
+
+var xhr = null;
+
+//on submit form
+function doSubmit() {
+	//check if cpr format is valid
+	if ( isValidCPR( $.cpr_number.value ) ) {
+		
+		//validate cpr with ajax call from cprvalidering.apiary.io if internet access available	    
+		if ( Titanium.Network.online ) {
+			
+			//show activity indicator
+			Alloy.Globals.activityIndicatorWnd.show({ 
+				message: 'Validating CPR number...',
+				onBack: function() {		
+					//abort ajax call
+					xhr.abort();
+					
+					//hide activity indicator
+					this.hide();
+				}
+			});
+			
+			//abort ajax call
+			if ( xhr )
+				xhr.abort();
+			
+			//create ajax call
+			xhr = Ti.Network.createHTTPClient({
+				onload: function(e) {
+					var cpr_valid = false;
+					try {
+						var json = JSON.parse(this.responseText);	
+						cpr_valid = json.person && json.person.valid && json.person.valid === true;			
+					} catch(error) {}
+					
+					//set cpr valid/!
+					cprValidated( cpr_valid );
+				},
+				onerror: function(e) {
+					//if no internet we assume the cpr is valid if the format is valid
+					cprValidated( true );
+				}
+			});
+			
+			//http://zoltan.web2it.net/cpr-validate.php?cpr=#CPR# -> http://cpr-validering.dk/api/api/validate
+			xhr.open('GET',"http://zoltan.web2it.net/cpr-validate.php?cpr="+$.cpr_number.value, true );
+			xhr.send();	
+		} else {
+			
+			//if no internet we assume the cpr is valid if the format is valid
+			cprValidated( true );
+		}
+		
+	} else {
+		cprValidated( false );	
 	}
 };
+
+//on window open
+$.index.addEventListener("open", function(evt) { 
+	//set action bar
+	Alloy.Globals.setActionBar( $.index );
+});
 
 $.index.open();
